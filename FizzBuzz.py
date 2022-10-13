2b7e0d547a29189e181706d918b3e796fda6ec0f
Henry E <60112299+henryelle@users.noreply.github.com>
2022-10-13 16:01:29 -0500

    optimized attempt

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,5 @@
 for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-        continue
-    if i % 3 == 0:
-        print("Fizz")
-        continue
-    if i % 5 == 0:
-        print("Buzz")
-        continue
-    print(i)
+    z = "FizzBuzz" if i % 3 == 0 and i % 5 == 0 else i
+    z = "Fizz" if i % 3 == 0 else i
+    z = "Buzz" if i % 5 == 0 else i
+    print(z)
